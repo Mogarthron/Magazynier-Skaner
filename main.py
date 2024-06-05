31a70b7ee9c521c2907603be0bf1526e8a192532
Mogathron <witluczynski@gmail.com>
2024-06-05 13:51:16 +0000

    pierwsze uruchomienie

--- main.py
+++ main.py
@@ -10,10 +10,27 @@ pytesseract.pytesseract.tesseract_cmd = tes_path
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     # Strona główna z przyciskiem do przesyłania zdjęć z aparatu
-    return render_template('index.html')
+    if request.method == 'POST':
+        if 'camera_image' not in request.files:
+            return jsonify({"error": "Brak pliku obrazu"}), 400
+        print("odczyt danych")
+        image = request.files['camera_image']
+        image_bytes = io.BytesIO(image.read())
+        pil_image = Image.open(image_bytes)
+        text = pytesseract.image_to_string(pil_image, lang='eng')
+        print(text.strip())
+        # Przetwarzanie tekstu, aby wyodrębnić numery
+        numbers = ''.join(filter(str.isdigit, text))
+        print("numbers", numbers, len(numbers))
+       
+        # return jsonify({"number": numbers})
+        return render_template('index.html', numbers=numbers,)
+    else:
+        # Renderowanie strony HTML z możliwością przesyłania zdjęć
+        return render_template('index.html', numbers="brak numeru")
 
 @app.route('/read-number', methods=['POST'])
 def read_number():
@@ -39,7 +56,7 @@ def upload_from_camera():
         image = request.files['camera_image']
         image_bytes = io.BytesIO(image.read())
         pil_image = Image.open(image_bytes)
-        text = pytesseract.image_to_string(pil_image, lang='pol')
+        text = pytesseract.image_to_string(pil_image, lang='eng')
 
         # Przetwarzanie tekstu, aby wyodrębnić numery
         numbers = ''.join(filter(str.isdigit, text))
@@ -47,7 +64,7 @@ def upload_from_camera():
         return jsonify({"number": numbers})
     else:
         # Renderowanie strony HTML z możliwością przesyłania zdjęć
-        return render_template('upload_from_camera.html')
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
