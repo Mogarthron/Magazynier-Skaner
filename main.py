5deb8727bd908e9ba3fcd097442c4fd77aa079bc
Mogathron <witluczynski@gmail.com>
2024-06-12 13:26:56 +0000

    kontrola czasu

--- main.py
+++ main.py
@@ -34,39 +34,22 @@ def index():
         # Renderowanie strony HTML z możliwością przesyłania zdjęć
         return render_template('index.html', numbers="brak numeru")
 
-# @app.route('/read-number', methods=['POST'])
-# def read_number():
-#     if 'image' not in request.files:
-#         return jsonify({"error": "Brak pliku obrazu"}), 400
 
-#     image = request.files['image']
-#     image_bytes = io.BytesIO(image.read())
-#     pil_image = Image.open(image_bytes)
-#     text = pytesseract.image_to_string(pil_image, lang='eng')
 
-#     # Przetwarzanie tekstu, aby wyodrębnić numery
-#     numbers = ''.join(filter(str.isdigit, text))
+@app.route("/kontrola_czasu", methods=["GET", "POST"])
+def kontrola_czasu():
+    user_name = "PAN ADAM"
+    procesy = [["ROZLADUNEK DOSTAWY NR PARTII: 20/01"],
+               ["KONTROLA JAKOSCI NR PARTII: 20/01"],
+               ["DOKLADANIE MEMORY NR PARTII: 20/01"],
+               ["DOKLADANIE OWAT NR PARTII: 20/01"],
+               ]
+    
+    if request.method == "POST":
+        print(request.form.keys())
+    
+    return render_template("kontrola_czasu.html", user_name=user_name, procesy=procesy)
 
-#     return jsonify({"number": numbers})
-
-# @app.route('/upload-from-camera', methods=['GET', 'POST'])
-# def upload_from_camera():
-#     if request.method == 'POST':
-#         if 'camera_image' not in request.files:
-#             return jsonify({"error": "Brak pliku obrazu"}), 400
-
-#         image = request.files['camera_image']
-#         image_bytes = io.BytesIO(image.read())
-#         pil_image = Image.open(image_bytes)
-#         text = pytesseract.image_to_string(pil_image, lang='eng')
-
-#         # Przetwarzanie tekstu, aby wyodrębnić numery
-#         numbers = ''.join(filter(str.isdigit, text))
-
-#         return jsonify({"number": numbers})
-#     else:
-#         # Renderowanie strony HTML z możliwością przesyłania zdjęć
-#         return render_template('index.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
