dabe512e2b74a7c763b10d262a6ab302c7dc5d04
Mogarthron <witluczynski@gmial.com>
2024-07-11 13:28:52 +0200

    backend

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ from app import app
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        app.run(debug=True, host="127.0.0.1", port=5000)
+        app.run(debug=True, host="0.0.0.0", port=5000)
     else:
         app.run(debug=False, host=sys.argv[1], port=sys.argv[2])
