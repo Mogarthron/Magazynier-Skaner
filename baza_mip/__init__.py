39c564fa4f5747c884a715fe4d773dc7ae83090f
Mogarthron <witluczynski@gmial.com>
2024-08-19 07:43:30 +0200

    backend

--- baza_mip/__init__.py
+++ baza_mip/__init__.py
@@ -5,20 +5,20 @@ from sqlalchemy import URL
 from sqlalchemy.orm import sessionmaker, declarative_base
 import json
 
-# with open("config.json", "r") as c:
-#     mip_url = json.load(c)["MIP_URL"]
+with open("config.json", "r") as c:
+    mip_url = json.load(c)["MIP_URL"]
 
-# url_obj = URL.create("mysql+mysqlconnector",
-#                      username=mip_url["username"],
-#                      password=mip_url["password"],
-#                      host=mip_url["host"],
-#                      port=mip_url["port"],
-#                      database=mip_url["database"]
-#                      )
+url_obj = URL.create("mysql+mysqlconnector",
+                     username=mip_url["username"],
+                     password=mip_url["password"],
+                     host=mip_url["host"],
+                     port=mip_url["port"],
+                     database=mip_url["database"]
+                     )
 
-# mip_engine = create_engine(url_obj, echo=False)
+mip_engine = create_engine(url_obj, echo=False)
 
-mip_engine = create_engine("sqlite:///BAZA_MIP.db", echo=False)
+# mip_engine = create_engine("sqlite:///BAZA_MIP.db", echo=False)
 
 Base = declarative_base()
 
