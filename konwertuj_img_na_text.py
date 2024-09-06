b49051ebc2eb4956b91375c8f858c5f8fa919eb3
Mogarthron <witluczynski@gmial.com>
2024-09-06 07:39:04 +0200

    backend

--- konwertuj_img_na_text.py
+++ konwertuj_img_na_text.py
@@ -10,16 +10,19 @@ import os
 def odczyt_numeru(request, username=None):
    
     image = request.files['camera_image']
-    image_bytes = io.BytesIO(image.read())
-    pil_image = Image.open(image_bytes)
-    # pil_image.save(f'./static/{username}_{dt.now().strftime("%Y-%m-%d_%H%M%S")}.jpg')
- 
-    # img_path = [x for x in os.listdir("./static") if username in x][-1]
-    # image = cv2.cvtColor(cv2.imread(os.path.join("./static", img_path)), cv2.COLOR_BGR2RGB)
-    image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_BGR2RGB)
+    try:
+        image_bytes = io.BytesIO(image.read())
+        pil_image = Image.open(image_bytes)
+        # pil_image.save(f'./static/{username}_{dt.now().strftime("%Y-%m-%d_%H%M%S")}.jpg')
+    
+        # img_path = [x for x in os.listdir("./static") if username in x][-1]
+        # image = cv2.cvtColor(cv2.imread(os.path.join("./static", img_path)), cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_BGR2RGB)
      
-    qreader = QReader()
-    decoded_text = qreader.detect_and_decode(image=image)
+        qreader = QReader()
+        decoded_text = qreader.detect_and_decode(image=image)
+    except:
+        decoded_text = ["BŁĄD ODCZYTU"]
 
     return decoded_text[0]
      
